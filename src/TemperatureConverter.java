85c6c906680ea30472c047f28aa43037af3d5009
blee4 <blee4@hawaii.edu>
2015-02-02 19:24:59 -1000

    Added secondArgCheck method

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -6,43 +6,17 @@ public class TemperatureConverter {
     public static void main(String[] args) {
         if (args[0].equals("-f")) {
             // Conversion from Fahrenheit to Celsius
-            if (args.length != 2) {
-                // Check if second argument exists
-                System.out.println ("Illegal second argument");
-                System.out.println ("Usage: TemperatureConverter <-f/-c> <int>");
-                System.exit(0);
-            }
-            else if (!isInt(args[1])) {
-                // Check if second argument is integer
-                System.out.println ("Illegal second argument");
-                System.out.println ("Usage: TemperatureConverter <-f/-c> <int>");
-                System.exit(0);
-            }
-            else {
-                int val = Integer.parseInt(args[1]);
-                double celsius = (val - 32) * 5 / 9;
-                System.out.printf("%d F is %f C.\n", val, celsius);
-            }
+            secondArgCheck (args);
+            int val = Integer.parseInt(args[1]);
+            double celsius = (val - 32) * 5 / 9;
+            System.out.printf("%d F is %f C.\n", val, celsius);
         }
         else if (args[0].equals("-c")) {
             // Conversion to Celsius from Fahrenheit
-            if (args.length != 2) {
-                // Check if second argument exists
-                System.out.println ("Illegal second argument");
-                System.out.println ("Usage: TemperatureConverter <-f/-c> <int>");
-                System.exit(0);
-            }
-            else if (!isInt(args[1])) {
-                // Check if second argument is integer
-                System.out.println ("Illegal second argument");
-                System.out.println ("Usage: TemperatureConverter <-f/-c> <int>");
-                System.exit(0);
-            }
-            else {
-                int val = Integer.parseInt(args[1]);
-                double fahrenheit = (val * 9 / 5) + 32;
-                System.out.printf("%d C is %f F.\n", val, fahrenheit);
-            }
+            secondArgCheck (args);
+            int val = Integer.parseInt(args[1]);
+            double fahrenheit = (val * 9 / 5) + 32;
+            System.out.printf("%d C is %f F.\n", val, fahrenheit);
         }
         else {
             System.out.println ("Illegal first argument");
@@ -58,5 +32,20 @@ public class TemperatureConverter {
             return false;
         }
         return true;
+    }
+
+    private static void secondArgCheck (String[] args) {
+        if (args.length != 2) {
+            // Check if second argument exists
+            System.out.println ("Illegal second argument");
+            System.out.println ("Usage: TemperatureConverter <-f/-c> <int>");
+            System.exit(0);
+        }
+        else if (!isInt(args[1])) {
+            // Check if second argument is integer
+            System.out.println ("Illegal second argument");
+            System.out.println ("Usage: TemperatureConverter <-f/-c> <int>");
+            System.exit(0);
+        }
     }
 }
