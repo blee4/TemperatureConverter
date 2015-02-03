07b75420b8a25592b0e2f89a4357b68ee5438152
blee4 <blee4@hawaii.edu>
2015-02-02 18:36:00 -1000

    Bug fixes

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -2,20 +2,22 @@
  * Created by SixDays on 2/2/15.
  */
 public class TemperatureConverter {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         double celsius, fahrenheit;
-        if (args.length == 1) {
-            System.out.println ("Usage: TemperatureConverter <base> <temp>");
+
+        if (args.length != 2) {
+            System.out.println ("Not enough arguments");
+            System.out.println ("Usage: TemperatureConverter <-f/-c> <temp>");
             System.exit(0);
         }
 
-        if (args[0].equals("-f")) {
+        if (args[1].equals("-f")) {
             // Conversion from Fahrenheit to Celsius
             int val = Integer.parseInt(args[1]);
             celsius = (val - 32) * 5/9;
             System.out.printf("%dF is %fC.\n", val, celsius);
         }
-        else if (args[0].equals("-c")) {
+        else if (args[1].equals("-c")) {
             // Conversion to Celsius from Fahrenheit
             int val = Integer.parseInt(args[1]);
             fahrenheit = (val * 9/5) + 32;
